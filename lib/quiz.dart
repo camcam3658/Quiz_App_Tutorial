0eb0791e27c9f2f230d01841652aa3e2fdbb5439
camcam3658 <gamegame1316@gmail.com>
2024-05-29 01:31:52 +0900

    クイズ結果画面まで実装

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,7 +1,9 @@
+import 'package:adv_basics/data/questions.dart';
 import 'package:flutter/material.dart';
 
 import 'package:adv_basics/start_screen.dart';
 import 'package:adv_basics/questions_screen.dart';
+import 'package:adv_basics/results_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -13,6 +15,7 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
   var activeScreen = 'start-screen';
 
   void switchScreen() {
@@ -21,14 +24,30 @@ class _QuizState extends State<Quiz> {
     });
   }
 
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        selectedAnswers = [];
+        activeScreen = 'results-screen';
+      });
+    }
+  }
+
   @override
   Widget build(context) {
     Widget screenWidget = StartScreen(switchScreen);
 
     if (activeScreen == 'questions-screen') {
-      screenWidget = const QuestionsScreen();
+      screenWidget = QuestionsScreen(
+        onSelectAnswer: chooseAnswer,
+      );
     }
 
+    if (activeScreen == 'results-screen') {
+      screenWidget = const ResultsScreen();
+    }
     return MaterialApp(
       home: Scaffold(
         body: Container(
